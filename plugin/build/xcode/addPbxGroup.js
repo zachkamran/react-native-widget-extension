b1b102a40b672538738ed4c6457b1542c6d8af0b
zach_kamran <zach@usesimple.ai>
2024-09-11 17:04:13 -0700

    add build

---
+++ plugin/build/xcode/addPbxGroup.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addPbxGroup = addPbxGroup;
+function addPbxGroup(xcodeProject, { targetName, widgetFiles, }) {
+    const { swiftFiles, intentFiles, otherFiles, assetDirectories, entitlementFiles, plistFiles, } = widgetFiles;
+    // Add PBX group
+    const { uuid: pbxGroupUuid } = xcodeProject.addPbxGroup([
+        ...swiftFiles,
+        ...intentFiles,
+        ...otherFiles,
+        ...entitlementFiles,
+        ...plistFiles,
+        ...assetDirectories,
+    ], targetName, targetName);
+    // Add PBXGroup to top level group
+    const groups = xcodeProject.hash.project.objects["PBXGroup"];
+    if (pbxGroupUuid) {
+        Object.keys(groups).forEach(function (key) {
+            if (groups[key].name === undefined && groups[key].path === undefined) {
+                xcodeProject.addToPbxGroup(pbxGroupUuid, key);
+            }
+        });
+    }
+}
---
+++ plugin/build/xcode/addProductFile.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addProductFile = addProductFile;
+function addProductFile(xcodeProject, { targetName, groupName }) {
+    const options = {
+        basename: `${targetName}.appex`,
+        // fileRef: xcodeProject.generateUuid(),
+        // uuid: xcodeProject.generateUuid(),
+        group: groupName,
+        explicitFileType: "wrapper.app-extension",
+        /* fileEncoding: 4, */
+        settings: {
+            ATTRIBUTES: ["RemoveHeadersOnCopy"],
+        },
+        includeInIndex: 0,
+        path: `${targetName}.appex`,
+        sourceTree: "BUILT_PRODUCTS_DIR",
+    };
+    const productFile = xcodeProject.addProductFile(targetName, options);
+    return productFile;
+}
